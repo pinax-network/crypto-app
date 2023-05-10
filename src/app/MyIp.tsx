8ad94cff1f4971d0e588f4963d864ec50c08656c
Denis Carriere <carriere.denis@gmail.com>
2023-05-10 12:06:06 -0400

    add cloudflare

--- src/app/MyIp.tsx
+++ src/app/MyIp.tsx
@@ -37,26 +37,22 @@ async function ipFromAWS() {
 
 async function fetcher() {
     const promises = [
+        // ipFromAWS(),
         ipFromCloudflare(),
-        // ipFromIpify(),
-        ipFromAWS(),
     ];
-    const ips = await Promise.any(promises);
-    return ips;
+    return Promise.any(promises);
     // return ips.find(ip => ip !== undefined);
 }
 
 export default function MyIP() {
-    const {data, error, isLoading} = useSwr("ip", fetcher);
+    const {data, error, isLoading} = useSwr("ip", ipFromCloudflare);
     // const ip = ipFromCookies() ?? "unknown";
     // const country = countryFromCookies() ?? "";
     return (
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
             <p className="fixed right-0 bottom-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-100 lg:p-4 lg:dark:bg-zinc-800/30">
-                My IP:
-                <br/>{data?.provider ?? ""}
-                <br/>{data?.ip ?? "unknown"}
-                <br/>{data?.loc ?? ""}
+                My IP from {data?.provider ?? ""}
+                <br/>{data?.ip ?? "unknown"} {data?.loc ?? ""}
             </p>
         </div>
     )
