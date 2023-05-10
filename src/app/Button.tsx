08ac914fa674d25e178170188300d53454278655
Denis Carriere <carriere.denis@gmail.com>
2023-05-09 21:35:28 -0400

    update tag

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -1,7 +1,6 @@
 
 import kv from "@vercel/kv"
-import { revalidatePath } from "next/cache";
-import { Suspense } from "react";
+import { revalidateTag } from "next/cache";
 
 export async function ButtonCounter(props: { name: string }) {
   const counter = await kv.get<number>(`Button:name:${props.name}`) || 0;
@@ -14,14 +13,14 @@ export default function Button(props: { name: string }) {
   async function increment() {
     'use server';
     await kv.incr(`Button:name:${props.name}`);
-    revalidatePath("/");
+    revalidateTag(`Button:name:${props.name}`);
   }
 
   return (
     <form action={increment}>
         <button className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
             {/* @ts-expect-error Server Component */}
-            {props.name} <Suspense><ButtonCounter name={props.name} /></Suspense>
+            {props.name} <ButtonCounter name={props.name} />
         </button>
     </form>
   )
