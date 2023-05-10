c406b1b7a1a006f32a185ea8faeb9b9b4b0d37ed
Denis Carriere <carriere.denis@gmail.com>
2023-05-10 19:14:34 -0400

    setcookies

--- middleware.ts
+++ middleware.ts
@@ -3,32 +3,44 @@ import { match } from '@formatjs/intl-localematcher'
 import { i18n } from './i18n-config'
 
 function getLocale( request: NextRequest ) {
+  const current = request.cookies.get('NEXT_LOCALE');
+  if ( current ) return current.value;
   const locale = request.headers.get('accept-language')?.split(',').map((l) => l.split(';')[0]);
   return match(locale ?? [], i18n.locales as any, i18n.defaultLocale);
 }
 
-export default async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-  const response = NextResponse.next();
-
-  // Add IP & Country to cookies
-  const country = request.geo?.country?.toLowerCase() || 'us'
-  const ip = request.ip || '';
+function setCookies( response: NextResponse, ip: string, country: string, locale: string ) {
   response.cookies.set('ip', ip);
   response.cookies.set('country', country);
+  response.cookies.set("NEXT_LOCALE", locale);
+}
+
+export default async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname
+  // const response = NextResponse.next();
+
+  // Add IP & Country to cookies
+  const country = request.geo?.country?.toLowerCase() || '';
+  const ip = request.ip || '';
 
   // Check if there is any supported locale in the pathname
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  const pathnameLocale = i18n.locales.find(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   )
 
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  const locale = getLocale(request)
 
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+  // Redirect if there is no locale
+  if (!pathnameLocale) {
+    const response = NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+    setCookies(response, ip, country, locale);
+    return response;
+  } else {
+    const response = NextResponse.next()
+    setCookies(response, ip, country, pathnameLocale);
+    return response;
   }
 }
 
