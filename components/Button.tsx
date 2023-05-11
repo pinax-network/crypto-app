5e75bb4f2e181222cfbff99020f7f910f56213fe
Denis Carriere <carriere.denis@gmail.com>
2023-05-11 12:30:48 -0400

    fix css

--- components/Button.tsx
+++ components/Button.tsx
@@ -13,7 +13,7 @@ export default function Button(props: { name: string }) {
   return (
     <form action={increment}>
       <input type="text" id="name" name="name" value={name} hidden={true} readOnly />
-      <button type="submit" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-400 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+      <button type="submit" className="mb-2 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-400 hover:bg-gray-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           {/* @ts-expect-error Server Component */}
           {name} <ButtonCounter name={name} />
       </button>
