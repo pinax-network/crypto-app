3ff6818fcfb18843ca84cc29189d9326b3220b7f
AyoNicku <nickhosi@hotmail.com>
2023-05-12 09:43:04 -0400

    localization, crypto data fetch

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,36 +1,105 @@
 import Image from 'next/image'
-import Button from '@/components/Button'
-import MyIP from '@/components/MyIp'
 import { Locale } from '@/i18n-config'
 import t from './Home.i18n'
+import axios from "axios";
+import { useRouter } from "next/router";
 
-export default function Home({ locale }: { locale: Locale }) {
+type Data = {
+  id:                               string;
+  symbol:                           string;
+  name:                             string;
+  image:                            string;
+  current_price:                    number;
+  market_cap:                       number;
+  market_cap_rank:                  number;
+  fully_diluted_valuation:          number;
+  total_volume:                     number;
+  high_24h:                         number;
+  low_24h:                          number;
+  price_change_24h:                 number;
+  price_change_percentage_24h:      number;
+  market_cap_change_24h:            number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply:               number;
+  total_supply:                     number;
+  max_supply:                       number;
+  ath:                              number;
+  ath_change_percentage:            number;
+  ath_date:                         Date;
+  atl:                              number;
+  atl_change_percentage:            number;
+  atl_date:                         Date;
+  roi:                              null;
+  last_updated:                     Date;
+}
+
+
+
+export default function Home({ locale }: { locale: Locale }, {data}) {
+  const router = useRouter();
+
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          { t[locale]["select-your-team"] }
-        </p>
-      </div>
+              <div className="title">
+                    <h1>Crypto Prices</h1>
+        </div>
 
-      <div className="relative flex place-items-center before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/pinax.png"
-          alt="Pinax Logo"
-          width={431}
-          height={240}
-          priority
-        />
-      </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <Button name=".gems" />
-          <Button name="Pinax" />
-          <Button name="Nation" />
-          <Button name={t[locale]["i-dont-know"] } />
-      </div>
+        <div className="cryptos">
+                
+                <div className="card">
+                    <div className="image">
+                        <img src={data[0].image} alt="" />
+                    </div>
+                    <h4>{data[0].name}</h4>
+                    <p>CA${data[0].current_price}</p>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src={data[1].image} alt="" />
+                    </div>
+                    <h4>{data[1].name}</h4>
+                    <p>CA${data[1].current_price}</p>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src={data[2].image} alt="" />
+                    </div>
+                    <h4>{data[2].name}</h4>
+                    <p>CA${data[2].current_price}</p>
+                </div>
+        </div>
+        <div className="currency">
+            <h2>Currency</h2>
+            <button onClick={() => router.push("/cryptos/usd")}>USD</button>
+            <button>CAD</button>
+        </div>
+        <div className="language">
+            <h2>Language</h2>
+            <button>English</button>
+            <button>Korean</button>
+            <button>Chinese</button>
 
-      <MyIP locale={locale} />
+        </div>
+      <h1>hello</h1>
+      <h1>{t[locale]["i-dont-know"]}</h1>
     </main>
   )
+}
+export async function getServerSideProps() {
+  // const crypto = await axios.get(
+  //   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=bitcoin%2Cethereum%2Ceos&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+  // );
+  // const data = crypto.data;
+  // return {
+  //   props: { data },
+  // };
+  const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=bitcoin%2Cethereum%2Ceos&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+  const data = await response.json()
+
+  return {
+    props: {
+      data: data
+    }
+  }
 }
