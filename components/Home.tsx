b1b707a765b4397621173af79eff9f22b73f58e2
AyoNicku <nickhosi@hotmail.com>
2023-05-15 09:37:22 -0400

    code cleanup

--- components/Home.tsx
+++ components/Home.tsx
@@ -9,21 +9,14 @@ import useSWR from 'swr'
 
 
 export default function Home( props: any ) {
-  // const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=bitcoin%2Cethereum%2Ceos&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
-  // const data = await response.json()
+
   const locale = props.locale
-  //const data = props.data
-  
   const [ currency, setCurrency] = useState("cad");
-  const [ currencyName, setCurrencyName] = useState("cad");
-
-
-
+  const [ currencyName, setCurrencyName] = useState("CAD$ ");
   const { data, error, isLoading } = useSWR(currency, fetcher)
 
-  const currentCurrency = "CA$";
-
   console.log(props)
+
   if(!data) return <></>;
 
   return (
@@ -31,44 +24,35 @@ export default function Home( props: any ) {
       <div className="title">
         <h1>Crypto Prices</h1>
       </div>
+
       <div className="cryptos">
-              
-        <div className="card">
-          <div className="image">
-              <img src={data[0].image} alt="" />
-          </div>
-          <h4>{data[0].name}</h4>
-          <p>{currencyName}{data[0].current_price}</p>
-        </div>
-        <div className="card">
-          <div className="image">
-              <img src={data[1].image} alt="" />
-          </div>
-          <h4>{data[1].name}</h4>
-          <p>{currencyName}{data[1].current_price}</p>
-        </div>
-        <div className="card">
-          <div className="image">
-              <img src={data[2].image} alt="" />
-          </div>
-          <h4>{data[2].name}</h4>
-          <p>{currencyName}{data[2].current_price}</p>
-        </div>
+        {data.map((data: { name: string, current_price:string, image: string}) => {
+          return (
+            <main>
+              <div className="image">
+                <img src={data.image} alt="" />
+              </div>
+              <h1>{data.name}</h1>
+              <h1>{currencyName}{data.current_price}</h1>
+            </main>
+          )})}
       </div>
 
       <button onClick={() => {setCurrency("usd");  setCurrencyName("US$ ")}} >
         USD
       </button>
+
       <button onClick={() => {setCurrency("cad");  setCurrencyName("CA$ ")}}>
         CAD
       </button>
-
+      
       <h1>hello</h1>
       <h1>{t[locale]["lang"]}</h1>
       <h1>{t[locale]["i-dont-know"]}</h1>
     </main>
   )
 }
+
 export async function fetcher(currency: string) {
   const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=bitcoin%2Cethereum%2Ceos&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
   const data = await response.json()
